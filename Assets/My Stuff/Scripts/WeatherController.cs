2caae5099e3347000ae406120a4bf08e35daa527
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-01-30 17:11:45 +1300

    Changed shadow shader

---
+++ Assets/My Stuff/Scripts/WeatherController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    public static WeatherController Singleton;
+    public Material ShadowMaterial;
+    public float SunAngle;
+    public float DayLength = 20;
+    int shadowTimeID;
+
+    private void Awake()
+    {
+        Singleton = this;
+        ShadowMaterial.SetFloat("_Sun_Angle", SunAngle);
+
+        shadowTimeID = Shader.PropertyToID("_Time_Of_Day");
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTime", 0, 0.1f);
+    }
+
+    void UpdateTime()
+    {
+        ShadowMaterial.SetFloat(
+            shadowTimeID,
+            NetworkManager.Singleton.ServerTime.TimeAsFloat / DayLength
+        );
+    }
+}
